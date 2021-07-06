8dd7689de807a1d763450469653fe004831a4d9d
jaros <jaroslav.prossv@centrum.cz>
2021-07-06 13:19:56 +0200

    toDo without fetch

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="">
+        <span :class="isChecked ? `line-through` : ``">{{ item.text }}</span>
+        <button @click="deleteSelf">DELETE</button>
+        <button @click="check">CHECK</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ToDoItem",
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            isChecked: false,
+        };
+    },
+    methods: {
+        check() {
+            this.isChecked = !this.isChecked;
+        },
+
+        deleteSelf() {
+            this.$emit("deleteItem", this.item.id);
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.line-through {
+    text-decoration: line-through;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="">
+        <ul>
+            <li v-for="(item, index) in toDos" :key="index">
+                <ToDoItem :item="item" @deleteItem="deleteItem" />
+            </li>
+        </ul>
+        <button @click="increment">CLICK</button>
+    </div>
+</template>
+
+<script>
+import ToDoItem from "./ToDoItem.vue";
+
+export default {
+    name: "ToDoList",
+    props: {},
+    components: {
+        ToDoItem,
+    },
+
+    data() {
+        return {
+            toDos: [
+                {
+                    id: "1",
+                    text: "todo",
+                },
+                {
+                    id: "2",
+                    text: "todo2",
+                },
+            ],
+        };
+    },
+
+    methods: {
+        increment() {
+            this.toDos.push("1");
+        },
+
+        deleteItem(id) {
+            let index = -1;
+            for (let i = 0; i < this.toDos.length; i++) {
+                if (this.toDos[i].id === id) {
+                    index = i;
+                    break;
+                }
+            }
+            this.toDos.splice(index, 1);
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
